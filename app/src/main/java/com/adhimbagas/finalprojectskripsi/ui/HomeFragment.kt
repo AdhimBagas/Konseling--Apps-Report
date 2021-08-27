c6d42f7e0cac036a103853d6058374549b2030a0
adhim <adhimbagas@gmail.com>
2021-08-27 21:20:34 +0700

    delate-try-first

--- app/src/main/java/com/adhimbagas/finalprojectskripsi/ui/HomeFragment.kt
+++ app/src/main/java/com/adhimbagas/finalprojectskripsi/ui/HomeFragment.kt
@@ -1,12 +1,12 @@
-package com.adhimbagas.finalprojectskripsi
+package com.adhimbagas.finalprojectskripsi.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.adhimbagas.finalprojectskripsi.R
 
 class HomeFragment : Fragment() {
 
--- app/src/main/java/com/adhimbagas/finalprojectskripsi/ui/LaporFragment.kt
+++ app/src/main/java/com/adhimbagas/finalprojectskripsi/ui/LaporFragment.kt
@@ -1,10 +1,11 @@
-package com.adhimbagas.finalprojectskripsi
+package com.adhimbagas.finalprojectskripsi.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.adhimbagas.finalprojectskripsi.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ArticleFragment.newInstance] factory method to
+ * Use the [LaporFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ArticleFragment : Fragment() {
+class LaporFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -32,7 +33,7 @@ class ArticleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+        return inflater.inflate(R.layout.fragment_lapor, container, false)
     }
 
     companion object {
@@ -42,12 +43,12 @@ class ArticleFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ArticleFragment.
+         * @return A new instance of fragment LaporFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-                ArticleFragment().apply {
+                LaporFragment().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
