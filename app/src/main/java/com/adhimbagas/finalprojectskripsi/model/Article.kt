d96512cc558375897ecb01b1d0080dcc5cfd33d2
adhim <adhimbagas@gmail.com>
2021-09-02 16:52:44 +0700

    add article model, article adapters, and viewbinding

---
+++ app/src/main/java/com/adhimbagas/finalprojectskripsi/model/Article.kt
@@ -0,0 +1,13 @@
+package com.adhimbagas.finalprojectskripsi.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Article(
+    var title: String? = null,
+    var imageArticle: String? = null,
+    var dateCreated: String? = null,
+    var content: String? = null
+
+) : Parcelable
